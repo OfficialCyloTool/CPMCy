86760b88da937e75bb87996674e5243f0f8285da
OfficialCyloTool <projectfreedom1210@gmail.com>
2025-03-20 09:19:03 +0800

    Update and rename CPMCy.py to CPMCY.py

--- CPMCY.py
+++ CPMCY.py
@@ -2,7 +2,7 @@ import requests
 from time import sleep
 BASE_URL: str = "https://cylotool1.squareweb.app/api"
 
-class CPMcy:
+class OfficialCyloTool:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
